071c9afafde330551a4970602842f671fdbd583d
rusel95 <ruslanpopesku95@gmail.com>
2017-02-21 22:03:54 +0200

    Data model finished + codegon error fixed

--- Dreamster/Item+CoreDataProperties.swift
+++ Dreamster/Item+CoreDataProperties.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Admin on 21.02.17.
 //  Copyright © 2017 rusel95. All rights reserved.
-//  This file was automatically generated and should not be edited.
 //
 
 import Foundation
--- Dreamster/ItemType+CoreDataClass.swift
+++ Dreamster/ItemType+CoreDataClass.swift
@@ -4,13 +4,12 @@
 //
 //  Created by Admin on 21.02.17.
 //  Copyright © 2017 rusel95. All rights reserved.
-//  This file was automatically generated and should not be edited.
 //
 
 import Foundation
 import CoreData
 
-@objc(ItemType)
+
 public class ItemType: NSManagedObject {
 
 }
