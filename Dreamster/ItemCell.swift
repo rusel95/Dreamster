103e96adac9d2dc55340a8655086ade8a5d2c8cb
rusel95 <ruslanpopesku95@gmail.com>
2017-02-22 09:40:38 +0200

    1) add tableViewDelegate/Data Source method to MainVC

---
+++ Dreamster/ItemCell.swift
@@ -0,0 +1,20 @@
+//
+//  ItemCell.swift
+//  Dreamster
+//
+//  Created by Admin on 22.02.17.
+//  Copyright © 2017 rusel95. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    
+    @IBOutlet weak var titile: UILabel!
+
+    @IBOutlet weak var price: UILabel!
+   
+    @IBOutlet weak var details: UILabel!
+}
