3389b6d7d23f973189379e20672feaf23d4d930c
rusel95 <ruslanpopesku95@gmail.com>
2017-02-22 11:25:30 +0200

    Continue and finished implementing of coradata methods

--- Dreamster/ItemCell.swift
+++ Dreamster/ItemCell.swift
@@ -12,9 +12,18 @@ class ItemCell: UITableViewCell {
 
     @IBOutlet weak var thumb: UIImageView!
     
-    @IBOutlet weak var titile: UILabel!
+    @IBOutlet weak var title: UILabel!
 
     @IBOutlet weak var price: UILabel!
    
     @IBOutlet weak var details: UILabel!
+    
+    func configureCell(item: Item){
+        
+        title.text = item.title
+        price.text = "$\(item.price)"
+        details.text = item.details
+        
+    }
+    
 }
--- Dreamster/MainVC.swift
+++ Dreamster/MainVC.swift
@@ -10,7 +10,7 @@ import UIKit
 import CoreData
 
 class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFetchedResultsControllerDelegate {
-
+    
     @IBOutlet weak var segment: UISegmentedControl!
     @IBOutlet weak var tableView: UITableView!
     
@@ -21,23 +21,50 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        attemptFetch()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemCell
+        configureCell(cell: cell, indexPath: indexPath as NSIndexPath)
+        
+        return cell
+    }
+    
+    func configureCell(cell: ItemCell, indexPath: NSIndexPath) {
+        
+        let item = controller.object(at: indexPath as IndexPath)
+        cell.configureCell(item: item)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if let sections = controller.sections {
+            let sectionsInfo = sections[section]
+            return sectionsInfo.numberOfObjects
+        }
         return 0
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
+        
+        if let sections = controller.sections {
+            return sections.count
+        }
+        
         return 0
+    }
+    
+    //to be shure that raws are 150px
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150.0
     }
     
     func attemptFetch() {
@@ -47,11 +74,13 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
         
         let controller = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
         
+        self.controller = controller
+        
         do {
             try controller.performFetch()
         } catch {
             let error = error as NSError
-            print("\(error)")
+            print("\n\n \(error)")
         }
         
     }
@@ -64,7 +93,7 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
     func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
         tableView.endUpdates()
     }
-
+    
     //Listening of changes (deletion, insertion)
     func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
         switch type {
@@ -82,8 +111,8 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
         case .update:
             if let indexPath = indexPath {
                 let cell = tableView.cellForRow(at: indexPath) as! ItemCell
+                configureCell(cell: cell, indexPath: indexPath as NSIndexPath)
             }
-            //We will implement updating a little bit later
             break
         case .move:
             if let indexPath = indexPath {
@@ -93,8 +122,7 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
                 tableView.insertRows(at: [indexPath], with: .fade)
             }
             break
+        }
     }
     
-    
 }
-
